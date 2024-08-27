5823d91ec4d0eca74de88958a32386590da655c0
ZengZ-233 <147756443+ZengZ-233@users.noreply.github.com>
2024-08-27 11:09:26 +0800

    Add files via upload

--- src/main/java/com/example/pokemon/Pokedex.java
+++ src/main/java/com/example/pokemon/Pokedex.java
@@ -41,7 +41,7 @@ public class Pokedex {
         String inputText = inputField.getText();
         String urlString = "https://wiki.52poke.com/wiki/%E5%AE%9D%E5%8F%AF%E6%A2%A6%E5%88%97%E8%A1%A8%EF%BC%88%E6%8C%89%E5%85%A8%E5%9B%BD%E5%9B%BE%E9%89%B4%E7%BC%96%E5%8F%B7%EF%BC%89";
 
-        configureSSL();
+//        configureSSL();
 
         try {
             URL url = new URL(urlString);
